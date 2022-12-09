c2aabc87c6f240364b69b229c87c028b57cc1cab
nitingupta5856 <nitingupta5856>
2022-12-09 14:14:39 +0530

    task-16

--- util/database.js
+++ util/database.js
@@ -2,8 +2,8 @@ const mysql =require('mysql2');
 const pool = mysql.createPool({
     host:'localhost',
     user:'root',
-    database:'node-complete',
-    password:'nodecomplete'
+    database:'node-completed',
+    password:'Aa@nitin5856'
 
 });
  module.exports=pool.promise();
